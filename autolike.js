cc01501d44ce2b91ec8b7b0b3b5222ab07daff5d
Aaron Cook <cookx770@umn.edu>
2019-01-23 11:41:36 -0600

    Autolike the first available post, now reloads and has random timeout

--- autolike.js
+++ autolike.js
@@ -1,9 +1,32 @@
-function clickFirstLike() {
-  var targetButton = document.getElementsByClassName('coreSpriteHeartOpen')[0];
-  console.log('Clicking this button:');
-  console.log(targetButton);
-  targetButton.click();
+// click the target'th post, or if already liked the first one after that post
+// if all posts have been liked, do nothing and reload
+function clickLike(target = 0) {
+  console.log('Attempting to like post ' + target.toString());
+  if (target >= document.getElementsByClassName('coreSpriteHeartOpen').length) {
+    console.log('All available posts liked, reloading');
+    location.reload(true);
+  }
+
+  var targetButton = document.getElementsByClassName('coreSpriteHeartOpen')[target];
+
+  if (targetButton.children[0].getAttribute('aria-label') === 'Like') {
+    console.log('Clicking this button:');
+    console.log(targetButton);
+    targetButton.click();
+    location.reload(true);
+  } else {
+    clickLike(target + 1);
+  }
+}
+
+// Get appropriate timeout value in ms
+function getTimeoutValue(likesPerHour = 90) {
+  randomDiff = Math.random() * 100 * 1000 - 50000;
+  baseLine = 1 / likesPerHour * 60 * 60 * 1000;
+  return baseLine + randomDiff;
 }
 
 console.log('InstaLikes is active');
-setTimeout(clickFirstLike, 5*1000);
+var likeTimeout = getTimeoutValue();
+console.log('Waiting ' + likeTimeout.toString() + ' ms');
+setTimeout(clickLike, likeTimeout, 0);
